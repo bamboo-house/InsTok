98d1d53e2634414ca1678ab79a5187ee98b94727
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-08-06 15:46:32 +0900

    Header追加

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core'
+import EcoIcon from '@material-ui/icons/Eco';
+
+const useStyles = makeStyles(() => ({
+  typographyStyles: {
+    flex: 1
+  }
+}));
+
+function Header() {
+  const classes = useStyles();
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <Typography className={classes.typographyStyles}>
+          Anthony sistilli
+        </Typography>
+        <EcoIcon/>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header
