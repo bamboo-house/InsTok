743f42e88c9b8af3a9c592e3ebfabe62e5801f26
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-08-07 09:20:52 +0900

    djangoのapi仮実装

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+@api_view(["GET"])
+def tiktok_api(resuest):
+  data = [
+    {
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit"
+    },
+    {
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenvel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    }
+  ]
+  return Response(data, status=status.HTTP_201_CREATED)
